f5190584271647d73ff148b0b8fd18df9ab59de4
krishpi <agrawalmuskan460@gmail.com>
2025-06-20 13:02:39 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountid=11447
+let accountEmail = "muskan@gmail.com"
+var accountCity= "jaipur"
+
+let accountstate;
+
+//accountid =33666 mot allowed
+//var create problem in block scope  and functional scope so use let always
+
+console.table([accountid,accountEmail,accountCity,accountstate])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name="muskan"
+let age=22
+let islogin = true
+
+//datatype -> int,string,boolean
+//null -> empty
+//undefined-> value not defined
+//object
+console.log(typeof 22)
+//typeof null->object
+//typeof undefined_>undefined
