b00fdc8f0c780e8bc77d18209fb448876316ceb6
krishpi <agrawalmuskan460@gmail.com>
2025-06-20 14:04:41 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+console.log("2">1) //->true bcoz 2 is treat as number
+console.log(null>0)    //false
+console.log(null==0)   //false boz == not convert string to number and null is treated as 0
+console.log(null>=0)  //true bcoz comparsion convert string to number
+console.log("2"===2)   //false coz === means strictly check equality with datatype
