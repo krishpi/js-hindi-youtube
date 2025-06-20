efe480d729924302772e3546a99c263230b8d1a6
krishpi <agrawalmuskan460@gmail.com>
2025-06-20 13:19:15 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+//"33"->33 can convert
+//"33abc" _>33 not a number
+
+let score = "33"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+let islogin = 1
+console.log(typeof islogin)
+let valueislogin = Boolean(islogin)
+console.log(typeof valueislogin)
+
+let someNumber =20
+console.log(typeof someNumber)
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
