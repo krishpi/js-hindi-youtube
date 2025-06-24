805cc9156b6be9bbce1aac829748ae8846d27e76
krishpi <agrawalmuskan460@gmail.com>
2025-06-24 11:35:09 +0000

    04_iife

---
+++ .devcontainer/03_basics/04_iife.js
@@ -0,0 +1,15 @@
+//iife->immediately invoked function expression
+
+//()-definition ()-execution
+//there is a problem due to global scope pollution ,so to remove it we use iifee
+
+(function fn(){
+    console.log(`db connected`);
+})(); //semicolon necessary to end funtion here
+
+//by arrow fun
+(() => {
+    console.log(`db connected two`);
+})()
+
+
