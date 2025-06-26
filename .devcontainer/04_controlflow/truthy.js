9fcfd5664f7d924a8124469fd4d976203a336555
krishpi <agrawalmuskan460@gmail.com>
2025-06-26 13:37:56 +0000

    truthy

---
+++ .devcontainer/04_controlflow/truthy.js
@@ -0,0 +1,28 @@
+const userEmail = "muskan@.ai"
+
+if(userEmail) {
+    console.log("got user email");
+}else {
+    console.log("do not have user email");
+}
+//empty string-> else condition run
+//empty array -> if condition run
+
+//falsy values :
+// false, 0, -0, bigint 0n, "", null, undefined, NaN
+
+//truthy values:
+// "0", 'false', " ", [], {}, function(){}
+
+
+//null coalescing operator(??)
+let val1;
+val1 = 5??10 //5
+val1 = null?? 10 //10
+val1 = undefined??15 //15
+val1 = null??10??20 //10
+console.log(val1);
+
+
+//ternary operator
+//condition ?true : false
