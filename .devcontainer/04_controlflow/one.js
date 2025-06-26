5753a6dce03c487a8f4b40c486c30bc76957ae78
krishpi <agrawalmuskan460@gmail.com>
2025-06-26 13:37:16 +0000

    ifelse

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,48 @@
+//if
+
+// if(true) {
+//     //run    
+// }  
+// false
+
+if(2 === "2"){
+    console.log("executed");
+} //not execute
+//if == is there  then execute
+
+const temp=41
+if(temp<50){
+    console.log("less than 50");
+} else {
+    console.log("temp is greater");
+}
+
+
+const balance =1000
+if(balance > 500)
+    console.log("test");
+
+if(balance < 500){
+         console.log("less than 500");
+} else if(balance <750){
+    console.log("less than 750");
+}else if(balance<900){
+    console.log("less than 900");
+}else {
+    console.log("less than 1200");
+}
+
+
+const userloggedin =true
+const debitcard = true
+if(userloggedin && debitcard){
+    console.log("allow for shopping");
+} else {
+    console.log("do not allow");
+}
+
+
+
+
+
+
