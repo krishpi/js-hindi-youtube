26d4781cfee6953595970482e4328af90a58ecd5
krishpi <agrawalmuskan460@gmail.com>
2025-06-27 16:21:32 +0000

    while and dowhile

---
+++ .devcontainer/05_iteration/two.js
@@ -0,0 +1,21 @@
+//while and do while
+
+let i=0
+while (i<=5) {
+    console.log("hello");
+    i++
+}
+
+let myArray =["a","b","c"]
+    let arr =0
+    while (arr<myArray.length) {
+        console.log(myArray[arr]);
+        arr++
+    }
+
+
+    let index=5
+    do {
+        console.log("hello");
+        index++
+    } while (index<5);
